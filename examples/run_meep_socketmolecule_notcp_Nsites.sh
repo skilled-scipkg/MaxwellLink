41a1a45ef76c1fc0a61ecf8bbe80d3cf5faa7502
Tao E. Li <t.e.li@outlook.com>
2025-10-03 14:02:41 -0400

    switch to light init to avoid huge overload of library import

--- examples/run_meep_socketmolecule_notcp_Nsites.sh
+++ examples/run_meep_socketmolecule_notcp_Nsites.sh
@@ -6,13 +6,6 @@ python meep_socketmolecule_notcp_Nsites.py $N &
 echo "After launching MEEP, sleeping 20 seconds to wait for the socket server to start..."
 sleep 20
 
-export OPENBLAS_NUM_THREADS=1
-export OMP_NUM_THREADS=1
-export MKL_NUM_THREADS=1
-export BLIS_NUM_THREADS=1
-export NUMEXPR_NUM_THREADS=1
-export MKL_DYNAMIC=FALSE
-
 for i in $(seq 1 $(($N-1))); do
     mxl_driver.py --model tls --unix --address mx10 --param "omega=0.242, mu12=187, orientation=2, pe_initial=1e-4" &
     sleep 0.2
